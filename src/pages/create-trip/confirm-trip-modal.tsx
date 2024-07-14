adfe8c2e77af691584863f854fa5f80f8f0494dc
Felipe Brito <felipebrito.lima94@gmail.com>
2024-07-14 19:49:50 -0300

    committed

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Mail, User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void
@@ -45,9 +46,9 @@ export function ConfirmTripModal({
               className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1" />
           </div>
 
-          <button type='submit' className='w-full bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-lime-400 '>
+          <Button type="submit" variant="primary" size="full">
             Confirmar criação da viagem
-          </button>
+          </Button>
         </form>
       </div>
     </div>
--- src/pages/create-trip/steps/destinatination-and-date-spep.tsx
+++ src/pages/create-trip/steps/destinatination-and-date-spep.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean
@@ -6,36 +7,37 @@ interface DestinationAndDateStepProps {
   openGuestsInput: () => void
 }
 
-
 export function DestinationAndDateStep({
   isGuestsInputOpen,
   closeGuestInput,
   openGuestsInput,
 }: DestinationAndDateStepProps) {
-    return(
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <div className='flex items-center gap-2 flex-1'>
-              <MapPin className='h-5 text-zinc-400' />
-              <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde voce vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-            </div>
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+      <div className='flex items-center gap-2 flex-1'>
+        <MapPin className='h-5 text-zinc-400' />
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde voce vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      </div>
 
-            <div className='flex items-center gap-2'>
-              <Calendar className='h-5 text-zinc-400' />
-              <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            </div>
+      <div className='flex items-center gap-2'>
+        <Calendar className='h-5 text-zinc-400' />
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+      </div>
 
-            <div className='w-px h-6 bg-zinc-800' />
+      <div className='w-px h-6 bg-zinc-800' />
 
-            {isGuestsInputOpen ? (
-              <button onClick={closeGuestInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-                Alterar local/data
-                <Settings2 className='size-5' />
-              </button>) :
-              <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-                Continuar
-                <ArrowRight className='size-5' />
-              </button>
-            }
-          </div>
-    )
+      {isGuestsInputOpen ? (
+        <Button onClick={closeGuestInput} variant="secondary">
+          Alterar local/data
+          <Settings2 className='size-5' />
+        </Button>
+      ) : (
+        <Button onClick={openGuestsInput} variant="primary">
+          Continuar
+          <ArrowRight className='size-5' />
+        </Button>
+      )
+      }
+    </div>
+  )
 }
